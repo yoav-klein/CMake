e651eafeceeab25bb2780b83432ae27c5d4ab443
Yoav Klein <yoavklein25@gmail.com>
2021-05-09 01:53:33 +0300

    Adding the math example - demonstrates a project with library and executable

---
+++ math/calculator/main.cpp
@@ -0,0 +1,10 @@
+
+#include <iostream>
+#include "math.h"
+
+int main()
+{
+	std::cout << "1 + 4 = " << add(1, 4) << std::endl;
+	
+	return 0;
+}
---
+++ math/libmath/src/math.cpp
@@ -0,0 +1,7 @@
+
+#include "math.h"
+
+int add(int a, int b)
+{
+	return a + b;
+}
